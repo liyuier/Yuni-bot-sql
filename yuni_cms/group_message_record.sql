5034eb589ea3e9d36d9f188a997209d4ba50bd2f
sagiri <sagiri@yui.com>
2024-04-08 00:17:13 +0800

    第一版数据库

---
+++ yuni_cms/group_message_record.sql
@@ -0,0 +1,41 @@
+/*
+ Navicat Premium Data Transfer
+
+ Source Server         : yuni
+ Source Server Type    : MySQL
+ Source Server Version : 80200 (8.2.0)
+ Source Host           : localhost:3306
+ Source Schema         : yuni_cms
+
+ Target Server Type    : MySQL
+ Target Server Version : 80200 (8.2.0)
+ File Encoding         : 65001
+
+ Date: 07/04/2024 23:52:43
+*/
+
+SET NAMES utf8mb4;
+SET FOREIGN_KEY_CHECKS = 0;
+
+-- ----------------------------
+-- Table structure for group_message_record
+-- ----------------------------
+DROP TABLE IF EXISTS `group_message_record`;
+CREATE TABLE `group_message_record`  (
+  `id` bigint NOT NULL AUTO_INCREMENT COMMENT 'id',
+  `time` datetime NOT NULL COMMENT '收到消息的时间戳',
+  `self_id` bigint NOT NULL COMMENT '收到消息的机器人 QQ 号',
+  `message_id` bigint NOT NULL COMMENT '消息ID',
+  `group_id` bigint NOT NULL COMMENT '群号',
+  `user_id` bigint NOT NULL COMMENT '发送者 QQ 号',
+  `anonymous` int NOT NULL DEFAULT 0 COMMENT '默认0，表示不是匿名信息；1表示是匿名信息',
+  `anonymous_id` bigint NULL DEFAULT NULL COMMENT '匿名用户ID',
+  `anonymous_name` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL COMMENT '匿名用户名称',
+  `anonymous_flag` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL COMMENT '匿名用户 flag，调用禁言API时传入',
+  `message` json NOT NULL COMMENT '消息内容',
+  `raw_message` longtext CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL COMMENT '原始消息内容',
+  `del_flag` int NOT NULL DEFAULT 0 COMMENT '删除标志，默认0，表示未删除；1表示删除',
+  PRIMARY KEY (`id`) USING BTREE
+) ENGINE = InnoDB AUTO_INCREMENT = 1 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci ROW_FORMAT = Dynamic;
+
+SET FOREIGN_KEY_CHECKS = 1;
---
+++ yuni_cms/private_message_record.sql
@@ -0,0 +1,37 @@
+/*
+ Navicat Premium Data Transfer
+
+ Source Server         : yuni
+ Source Server Type    : MySQL
+ Source Server Version : 80200 (8.2.0)
+ Source Host           : localhost:3306
+ Source Schema         : yuni_cms
+
+ Target Server Type    : MySQL
+ Target Server Version : 80200 (8.2.0)
+ File Encoding         : 65001
+
+ Date: 07/04/2024 23:52:50
+*/
+
+SET NAMES utf8mb4;
+SET FOREIGN_KEY_CHECKS = 0;
+
+-- ----------------------------
+-- Table structure for private_message_record
+-- ----------------------------
+DROP TABLE IF EXISTS `private_message_record`;
+CREATE TABLE `private_message_record`  (
+  `id` bigint NOT NULL AUTO_INCREMENT COMMENT 'id',
+  `time` datetime NOT NULL COMMENT '收到消息的时间戳',
+  `self_id` bigint NOT NULL COMMENT '收到消息的机器人QQ号',
+  `message_id` bigint NOT NULL COMMENT '消息ID',
+  `user_id` bigint NOT NULL COMMENT '发送者QQ号',
+  `nickname` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL COMMENT '发送者昵称',
+  `message` json NOT NULL COMMENT '消息内容',
+  `raw_message` longtext CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL COMMENT '原始消息内容',
+  `del_flag` int NOT NULL DEFAULT 0 COMMENT '删除标志，默认0，表示未删除；1表示删除',
+  PRIMARY KEY (`id`) USING BTREE
+) ENGINE = InnoDB AUTO_INCREMENT = 1 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci ROW_FORMAT = Dynamic;
+
+SET FOREIGN_KEY_CHECKS = 1;
